822674281b3a1b2bd19312c191187d079b22d43d
clifford <clifford@noukod.com>
2019-01-12 16:59:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, world!"
+  end
+
+  get '/name' do
+    "My name is Clifford LOUIS"
+  end
+
+  get '/hometown' do
+    "My hometown is Delmas"
+  end
+  get '/favorite-song' do
+    "My favorite song is I surrender, Hillsong"
+  end
 end
